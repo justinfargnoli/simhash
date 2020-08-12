9ef27d06188ae33a258896a5185927ad7c03ac8d
Justin Fargnoli <justinfargnoli@gmail.com>
2020-08-11 23:37:16 -0400

    Add tests

--- simhash_test.go
+++ simhash_test.go
@@ -1,1 +1,24 @@
 package simhash
+
+import "testing"
+
+func TestOnlineSimhash(t *testing.T) {
+	vectors := [][]float64{
+		{0.0, 2.0, 3.3, -4.2},
+		{0.0, -2.0, 3.3, -4.2},
+		{0.0, 2.0, -3.3, -4.2},
+	}
+	online := NewOnline(300, uint(len(vectors[0])))
+	for _, v := range vectors {
+		online.Simhash(v)
+	}
+}
+
+func TestOffline(t *testing.T) {
+	vectors := [][]float64{
+		{0.0, 2.0, 3.3, -4.2},
+		{0.0, -2.0, 3.3, -4.2},
+		{0.0, 2.0, -3.3, -4.2},
+	}
+	Offline(vectors, 300)
+}
