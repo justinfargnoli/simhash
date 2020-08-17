764edc9a46045fb37afae50ecb6434d3cb85c5d0
Justin Fargnoli <justinfargnoli@gmail.com>
2020-08-17 14:34:12 -0400

    Add type Bit

--- simhash.go
+++ simhash.go
@@ -17,35 +17,35 @@ func NewOnline(hyperplaneCount, dim uint) Online {
 }
 
 // Hash constructs a simhash data sketch of the given vector
-func (o Online) Hash(vector []float64) *Simhash {
+func (o Online) Hash(vector []float64) *[]Bit {
 	return NewSimhash(*o.hyperplanes, vector)
 }
 
 // Offline sketches each vector in an offline fashion
-func Offline(vectors [][]float64, hyperplaneCount uint) []Simhash {
-	simhashs := make([]Simhash, len(vectors))
+func Offline(vectors [][]float64, hyperplaneCount uint) *[][]Bit {
+	simhashs := make([][]Bit, len(vectors))
 	hyperplanes := NewHyperplanes(hyperplaneCount, uint(len(vectors[0])))
 	for i, vector := range vectors {
 		simhashs[i] = *NewSimhash(hyperplanes, vector)
 	}
-	return simhashs
+	return &simhashs
 }
 
-// Simhash is the simhash data sketch of an []float64
-type Simhash []uint8
+// Bit represents a bit in an element's hash bit array
+type Bit uint8
 
 // NewSimhash constructs a simhash data sketch of the given vector
-func NewSimhash(hyperplanes []Hyperplane, vector []float64) *Simhash {
-	simhash := make(Simhash, len(hyperplanes))
+func NewSimhash(hyperplanes []Hyperplane, vector []float64) *[]Bit {
+	simhash := make([]Bit, len(hyperplanes))
 	for i, hyperplane := range hyperplanes {
 		var dotProduct float64 // the dot product of hyperplanes[i] and vector
 		for j, v := range vector {
 			dotProduct += hyperplane[j] * v
 		}
 		if dotProduct >= 0 {
-			simhash[i] = uint8(1)
+			simhash[i] = Bit(1)
 		} else {
-			simhash[i] = uint8(0)
+			simhash[i] = Bit(0)
 		}
 	}
 	return &simhash
